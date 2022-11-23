19d1f31379ef672ab2de628d09df1b72ec439116
codenol <nanoparser@gmail.com>
2022-11-23 12:43:45 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Block from './components/Block.vue'
+</script>
+
+<template>
+  <Block msg="Ready for action!" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+<template>
+  <div class="flex bg-white rounded-xl items-center text-center max-w-sm m-4 mx-auto p-6"> 
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+
+    },
+    methods:{
+
+    },
+    props:{
+      msg: String,
+    }
+  }
+</script>
+
+<style scoped>
+</style>
